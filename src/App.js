4ae840070ff439e7808d6f3cf53f22d1fed6e5a3
GabrielC <gabriel.camargo96@hotmail.com.br>
2022-08-09 16:28:25 -0300

    chore: add signup component page to app
    Use the router to render component on route /sign-up.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 import GlobalStyle from "./themes/GlobalStyle";
+
 import Timeline from "./pages/Timeline.js";
+import Signup from "./pages/Signup";
 
 export default function App(){
     return(
@@ -9,7 +11,8 @@ export default function App(){
             <GlobalStyle />
             <BrowserRouter>
                 <Routes>
-                    <Route path="/timeline" element={<Timeline />}/>
+                    <Route path="/timeline" element={<Timeline />} />
+                    <Route path="/sign-up" element={<Signup />} />
                 </Routes>
             </BrowserRouter>    
             

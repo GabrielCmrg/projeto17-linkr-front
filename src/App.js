bf6713bc2a0bf7f1ce54286ddc858cba715d716e
PauloGabrieel <gabrieeel.c@gmail.com>
2022-08-09 08:25:40 -0300

    feat: import timeline route in app.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+
 import GlobalStyle from "./themes/GlobalStyle";
+import Timeline from "./pages/Timeline.js";
+
 export default function App(){
     return(
         <>
             <GlobalStyle />
             <BrowserRouter>
                 <Routes>
-                    
+                    <Route path="/timeline" element={<Timeline />}/>
                 </Routes>
             </BrowserRouter>    
             

de89108a640e721e71e394b7c3a2e5f23d38ec34
GabrielC <gabriel.camargo96@hotmail.com.br>
2022-08-09 17:47:03 -0300

    feat: add styles to signup form

--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import styled from "styled-components";
 
 export default function Signup() {
@@ -9,7 +10,7 @@ export default function Signup() {
                     <h2>save, share and discover the best links on the web</h2>
                 </div>
             </Brand>
-            <div>
+            <Form>
                 <form>
                     <input type="email" placeholder="e-mail" id="email" />
                     <input type="password" placeholder="password" id="password" />
@@ -17,7 +18,8 @@ export default function Signup() {
                     <input type="url" placeholder="picture url" id="url" />
                     <button type="submit">Sign Up</button>
                 </form>
-            </div>
+                <Link to="/">Switch back to log in</Link>
+            </Form>
         </Container>
     );
 };
@@ -26,7 +28,7 @@ const Container = styled.div`
     width: 100vw;
     height: 100vh;
     display: flex;
-`
+`;
 
 const Brand = styled.div`
     background-color: #151515;
@@ -37,6 +39,7 @@ const Brand = styled.div`
     align-items: center;
     color: white;
     font-weight: 700;
+    flex-shrink: 0;
 
     h1 {
         font-size: 106px;
@@ -48,4 +51,56 @@ const Brand = styled.div`
         font-family: 'Oswald', sans-serif;
         width: 442px;
     }
-`
+`;
+
+const Form = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    padding: 54px;
+
+    form {
+        width: 100%;
+
+        input {
+            width: 100%;
+            height: 65px;
+            border-radius: 6px;
+            border: none;
+            background-color: white;
+            color: black;
+            font-size: 27px;
+            font-family: 'Oswald', sans-serif;
+            margin-bottom: 13px;
+            padding: 17px;
+
+            ::placeholder {
+                color: #9F9F9F;
+                opacity: 1;
+            }
+        }
+
+        button {
+            width: 100%;
+            height: 65px;
+            border-radius: 6px;
+            border: none;
+            background-color: #1877F2;
+            color: white;
+            font-size: 27px;
+            font-family: 'Oswald', sans-serif;
+            margin-bottom: 14px;
+            
+            :hover {
+                filter: brightness(1.2);
+                cursor: pointer;
+            }
+        }
+    }
+
+    a {
+        font-family: 'Lato', sans-serif;
+        color: white;
+    }
+`;

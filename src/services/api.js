d0900a255d44e0323ee3a7f14044b5f3d499b632
PauloGabrieel <gabrieeel.c@gmail.com>
2022-08-12 11:49:04 -0300

    fix: change env variable name

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const  REACT_APP_API_URL  = "http://localhost:4000";
+const  {REACT_APP_API_URL}  = process.env;
 console.log(REACT_APP_API_URL);
 
 export async function signupRequest(email, name, password, picUrl) {

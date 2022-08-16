a369f9bcdb55997a62ab66e99350f00bfa3e8890
Vitor Carvalho Matos <vitorcarvalhomatos@gmail.com>
2022-08-16 17:03:08 -0300

    inicio da construção da logica do loadMore

--- src/pages/Timeline.js
+++ src/pages/Timeline.js
@@ -1,19 +1,23 @@
 import styled from "styled-components";
 import React from "react";
 import { useNavigate } from "react-router-dom";
+import useInterval from "use-interval";
 
 import Header from "../components/Header";
 import SearchBar from "../components/SearchBar";
-import PublicationForm  from "../components/PublicationForm.js";
+import PublicationForm from "../components/PublicationForm.js";
 import Publication from "../components/Publication";
 import Trending from "../components/Trending";
 
 import { getAllPostRequest } from "../services/api";
 
 import ApplicationContext from "../contexts/ApplicationContext.js";
+import { FiRefreshCw } from "react-icons/fi";
+
 
 export default function Timeline() {
-    const [posts, setPosts] = React.useState(null);
+    const [posts, setPosts] = React.useState("");
+    const [visiblePosts, setVisiblePosts] = React.useState("");
     const { userToken } = React.useContext(ApplicationContext);
     const navigate = useNavigate();
 
@@ -23,40 +27,47 @@ export default function Timeline() {
         }
     };
     React.useEffect(() => {
-        if(!userToken){
-            navigate("/",{replace:true});
+        if (!userToken) {
+            navigate("/", { replace: true });
             return;
         };
-        async function data(){
+        async function data() {
             const response = await getAllPostRequest(config);
-            if(response.status === 200) {
-                setPosts([...response.data]);
+            if (response.status === 200) {
+                setVisiblePosts([...response.data]);
             } else {
                 alert("An error occured while trying to fetch the posts, please refresh the page")
             };
         };
         data()
-        
+
         // eslint-disable-next-line react-hooks/exhaustive-deps
-    },[]);
-    
-    function checkForPosts (){
-        if(posts === null){
+    }, []);
+
+    useInterval(async () => {
+        const response = await getAllPostRequest(config);
+        if (response.status === 200) {
+            setPosts([...response.data]);
+        }
+    }, 15000);
+
+    function checkForPosts() {
+        if (!visiblePosts) {
             return (
                 <TextContainer>
                     <h2>Loading...</h2>
                 </TextContainer>
             );
-        } else if (posts.length === 0) {
+        } else if (visiblePosts.length === 0) {
             return (
                 <TextContainer>
                     <h2>There are no posts yet</h2>
                 </TextContainer>
             );
-        }else{
-            return(
-                posts.map(item=>(
-                    <Publication  
+        } else {
+            return (
+                visiblePosts.map(item => (
+                    <Publication
                         key={item.id}
                         userLiked={item.userliked}
                         firstLike={item.firstlike}
@@ -76,7 +87,28 @@ export default function Timeline() {
             );
         };
     };
+
+    function renderLoadMoreButton() {
+        if (posts.length - visiblePosts.length > 1) {
+            return (
+                <LoadMoreButton onClick={() => setVisiblePosts(posts)}>
+                    {`${posts.length - visiblePosts.length} new posts, load more!`}
+                </LoadMoreButton>
+            )
+        }
+        else if (posts.length - visiblePosts.length > 0) {
+            return (
+                <LoadMoreButton onClick={() => setVisiblePosts(posts)}>
+                    <span>{`${posts.length - visiblePosts.length} new posts, load more!`}</span>
+                    <FiRefreshCw />
+                </LoadMoreButton>
+            )
+        }
+        return ""
+    }
+
     const renderPosts = checkForPosts();
+
     return (
         <TimelineContainer>
             <Header />
@@ -85,6 +117,7 @@ export default function Timeline() {
                 <div>
                     <Title>timeline</Title>
                     <PublicationForm />
+                    {renderLoadMoreButton()}
                     {renderPosts}
                 </div>
                 <div>
@@ -124,5 +157,25 @@ const TextContainer = styled.div`
     h2{
         font: 400 20px 'Oswald', sans-serif;
         color: #FFFFFF;
+    }
+`;
+
+const LoadMoreButton = styled.button`
+    width:100%;
+    padding: 16px 18px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    background-color: #1877F2;
+    box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+    border-radius: 16px;
+    border: none;
+    font-weight: 400;
+    font-size: 16px;
+    font-family: 'Lato';
+    color: #FFFFFF;
+
+    span{
+        margin-right:10px;
     }
 `;

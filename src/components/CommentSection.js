f14a2d44ff04ba35721d7ee8e6d2644efb04cc02
GabrielC <gabriel.camargo96@hotmail.com.br>
2022-08-18 20:45:28 -0300

    feat: add input and style

--- src/components/CommentSection.js
+++ src/components/CommentSection.js
@@ -8,7 +8,10 @@ export default function CommentSection() {
 
     return (
         <Background>
-            <Avatar src={userImage} alt="User" />
+            <CommentBox>
+                <Avatar src={userImage} alt="User" />
+                <Input placeholder="write a comment..." />
+            </CommentBox>
         </Background>
     );
 };
@@ -19,9 +22,32 @@ const Background = styled.div`
     padding: 10px 20px;
 `;
 
+const CommentBox = styled.div`
+    display: flex;
+    padding: 5px;
+    gap: 15px;
+`;
+
 const Avatar = styled.img`
     height: 39px;
     width: 39px;
     border-radius: 50%;
     object-fit: cover;
+`;
+
+const Input = styled.input`
+    width: 100%;
+    height: 39px;
+    background-color: #252525;
+    border-radius: 8px;
+    border: none;
+    padding: 10px 15px;
+    color: white;
+
+    ::placeholder {
+        font-family: 'Lato', sans-serif;
+        font-style: italic;
+        font-size: 14px;
+        color: #575757;
+    }
 `;

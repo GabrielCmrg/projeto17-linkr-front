17ed3c0810e0e39e68a45a93b526ef697a2c06d2
GabrielC <gabriel.camargo96@hotmail.com.br>
2022-08-18 20:34:14 -0300

    feat: add avatar to comment section

--- src/components/CommentSection.js
+++ src/components/CommentSection.js
@@ -1,13 +1,27 @@
 import styled from "styled-components";
+import React from "react";
+
+import ApplicationContext from "../contexts/ApplicationContext";
 
 export default function CommentSection() {
+    const { userImage, userToken } = React.useContext(ApplicationContext);
+
     return (
-        <Background />
+        <Background>
+            <Avatar src={userImage} alt="User" />
+        </Background>
     );
 };
 
 const Background = styled.div`
     background-color: #1E1E1E;
-    height: 50px;
     border-radius: 0 0 16px 16px;
+    padding: 10px 20px;
+`;
+
+const Avatar = styled.img`
+    height: 39px;
+    width: 39px;
+    border-radius: 50%;
+    object-fit: cover;
 `;
